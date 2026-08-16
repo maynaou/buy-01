81f5a6dfcc1fd89def4e89915e3072f47b99a0a6
maynaou <mohssinaynaou874@gmail.com>
2026-08-16 12:40:49 +0100

    fix worning

--- security-service/src/main/java/com/example/security_service/dto/RegisterRequest.java
+++ security-service/src/main/java/com/example/security_service/dto/RegisterRequest.java
@@ -1,7 +1,6 @@
 package com.example.security_service.dto;
 
 import com.example.security_service.enums.Role;
-
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
