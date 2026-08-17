04b166049300c8bfb5fd917bd2ca6e6c492f42c7
hkouki <koukihamza469@gmail.com>
2026-08-17 17:14:20 +0100

    auth

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Register } from './features/auth/register/register';
+import { Login } from './features/auth/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'register',
+        component: Register
+    },
+    {
+        path: 'login',
+        component: Login
+    }
+];
