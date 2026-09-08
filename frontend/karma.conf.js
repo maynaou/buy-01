536ef9fa7e8b1e711d3770877c771db5e5703998
maynaou <mohssinaynaou874@gmail.com>
2026-09-08 15:32:36 +0100

    testtt

---
+++ frontend/karma.conf.js
@@ -0,0 +1,51 @@
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+
+    client: {
+      jasmine: {
+        // options Jasmine
+      },
+      clearContext: false
+    },
+
+    jasmineHtmlReporter: {
+      suppressAll: true
+    },
+
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' }
+      ]
+    },
+
+    reporters: ['progress', 'kjhtml'],
+
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: [
+          '--no-sandbox',
+          '--disable-gpu',
+          '--disable-dev-shm-usage'
+        ]
+      }
+    },
+
+    browsers: ['ChromeHeadlessNoSandbox'],
+
+    restartOnFileChange: true
+  });
+};
